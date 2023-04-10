4c24941fc4bd7feaaabf1ec5023d790d7416a46c
Jonle <jonas.lemmens@student.kuleuven.be>
2023-04-10 15:40:36 +0200

    Added all shapes + start dragging shapes

--- js/tangram.js
+++ js/tangram.js
@@ -1,9 +1,11 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
+var vierkant;
+vierkant= new component(30, 30, "orange", 10, 120);
 //De grootte van het tangramvierkant:
 var zijde = 100;
-var z_vierkant = 0.5*Math.sqrt(2*zijde*zijde) 
+var z_vierkant = zijde/(2*Math.sqrt(2))
 draw();
 
 //De verschillende vormen
@@ -37,9 +39,61 @@ function draw(){
     //Parallellogram
     ctx.fillStyle = "yellow";
     ctx.beginPath();
-    ctx.moveTo(0, 0);
-    ctx.lineTo(Math.cos(z_vierkant),Math.sin(z_vierkant));
-    ctx.lineTo(Math.cos(z_vierkant)+zijde/2,Math.sin(z_vierkant));
-    ctx.lineTo(zijde/2,0);
+    var place = 100;
+    //ctx.save();
+    //ctx.translate(place,place);
+    //ctx.rotate(180*Math.PI/180);
+    ctx.moveTo(place, place);
+    ctx.lineTo(place+zijde/2,place);
+    ctx.lineTo(place+zijde/2+z_vierkant*Math.cos(45*Math.PI/180),place+z_vierkant*Math.sin(45*Math.PI/180));
+    ctx.lineTo(place+z_vierkant*Math.cos(45*Math.PI/180),place+z_vierkant*Math.sin(45*Math.PI/180));  
+    ctx.lineTo(place,place);
     ctx.fill();
+    //ctx.restore();
 }
+
+function component(width, height, color, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;    
+    ctx.fillStyle = color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+}
+
+triangle.onmousedown = function(event) {
+
+    let shiftX = event.clientX - triangle.getBoundingClientRect().left;
+    let shiftY = event.clientY - triangle.getBoundingClientRect().top;
+  
+    triangle.style.position = 'absolute';
+    triangle.style.zIndex = 1000;
+    document.body.append(triangle);
+  
+    moveAt(event.pageX, event.pageY);
+  
+    // moves the triangle at (pageX, pageY) coordinates
+    // taking initial shifts into account
+    function moveAt(pageX, pageY) {
+      triangle.style.left = pageX - shiftX + 'px';
+      triangle.style.top = pageY - shiftY + 'px';
+    }
+  
+    function onMouseMove(event) {
+      moveAt(event.pageX, event.pageY);
+    }
+  
+    // move the triangle on mousemove
+    document.addEventListener('mousemove', onMouseMove);
+  
+    // drop the triangle, remove unneeded handlers
+    triangle.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      triangle.onmouseup = null;
+    };
+  
+  };
+  
+  triangle.ondragstart = function() {
+    return false;
+  };
