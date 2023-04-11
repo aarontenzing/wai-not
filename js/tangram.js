9bb4ea266bb2813c05d1d81ea29db8ccb83ef13d
Jonle <jonas.lemmens@student.kuleuven.be>
2023-04-11 20:20:20 +0200

    Beweging met muis

--- js/tangram.js
+++ js/tangram.js
@@ -1,15 +1,140 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
+//Canvas waarden
+canvas.width = window.innerWidth - 30;
+canvas.height = window.innerHeight - 10;
+
+var canvas_width = canvas.width;
+var canvas_height = canvas.height;
+
+let offset_x;
+let offset_y;
+
+let get_offset = function(){
+  let canvas_offsets = canvas.getBoundingClientRect();
+  offset_x = canvas_offsets.left;
+  offset_y = canvas_offsets.top;
+}
+
+get_offset();
+window.onscroll = function(){get_offset();}
+window.onresize = function(){get_offset();}
+canvas.onresize = function(){get_offset();}
+
+//Vormen
+let shapes = [];
+let current_shape_index = null;
+let drag = false;
+let startX;
+let startY;
+
+shapes.push({x : 200, y : 50, width: 100, height: 100 , color: 'red'});
+
+//Tekenvorm
+function draw_shapes(){
+  ctx.clearRect(0,0,canvas_width,canvas_height);
+  for(let shape of shapes){
+    console.log(shape.x+shape.y);
+    ctx.fillStyle = shape.color;
+    ctx.fillRect(shape.x,shape.y,shape.width,shape.height);
+  }
+}
+draw_shapes();
+
+let is_mouse_in_shape = function(x,y,shape){
+  let shape_left = shape.x;
+  let shape_right = shape.x + shape.width;
+  let shape_top = shape.y;
+  let shape_bottom = shape.y + shape.height;
+
+  if(x > shape_left && x < shape_right && y > shape_top && y < shape_bottom){
+    return true;
+  }
+  return false;
+}
+
+let mouse_down = function(event){
+
+  event.preventDefault();
+
+  startX = parseInt(event.clientX - offset_x);
+  startY = parseInt(event.clientY - offset_y);
+
+  let index = 0 ;
+  for(let shape of shapes){
+
+    if(is_mouse_in_shape(startX,startY,shape)){
+      console.log('yes');
+      current_shape_index = index;
+      console.log(current_shape_index);
+      drag=true;
+      return;
+    }else{
+      console.log('no');
+    }
+    index++;
+  }
+}
+
+let mouse_up = function(event){
+  if(!drag){
+    return;
+  }
+  event.preventDefault();
+  drag=false;
+}
+
+let mouse_out = function(event){
+  if(!drag){
+    return;
+  }
+  event.preventDefault();
+  drag=false;
+}
+
+let mouse_move = function(event){
+  if(!drag){
+    return;
+  }else{
+    event.preventDefault();
+    let mouseX = parseInt(event.clientX - offset_x);
+    let mouseY = parseInt(event.clientY - offset_y);
+
+    let dx = mouseX - startX;
+    let dy = mouseY - startY;
+
+    console.log(dx,dy);
+    let current_shape = shapes[current_shape_index];
+    
+    current_shape.x += dx;
+    current_shape.y += dy;
+    
+    //clear it and redraw
+    draw_shapes();
+
+    startX = mouseX;
+    startY = mouseY;
+
+  }
+}
+
+//eventlisteners
+canvas.onmousedown = mouse_down;
+canvas.onmouseup = mouse_up;
+canvas.onmouseout = mouse_out;
+canvas.onmousemove = mouse_move;
+
+
+
+/*
 var vierkant;
 vierkant= new component(30, 30, "orange", 10, 120);
 //De grootte van het tangramvierkant:
 var zijde = 100;
 var z_vierkant = zijde/(2*Math.sqrt(2))
 
-let drag = false;
-let startX;
-let startY;
+
 draw();
 
 //De verschillende vormen
@@ -65,80 +190,4 @@ function component(width, height, color, x, y) {
     ctx.fillRect(this.x, this.y, this.width, this.height);
 }
 
-let is_mouse_in_shape = function(x,y,shape){
-  let shape_left = shape.x;
-  let shape_right = shape.x + shape.width;
-  let shape_top = shape.x + shape.width;
-  let shape_bottom = shape.y + shape.height;
-
-  if(x>shape_left && x < shape_right && y>shape_top && y<shape_bottom){
-    return true;
-  }
-  return false;
-}
-
-let mouse_down = function(event){
-
-  event.PreventDefault();
-
-  startX = parseInt(event.clientX);
-  startY = parseInt(event.clientY);
-
-  if(is_mouse_in_shape(startX,startY,vierkant)){
-    console.log('yes');
-    drag=true;
-    return;
-  }else{
-    console.log('no');
-  }
-}
-
-let mouse_up = function(event){
-  event.PreventDefault();
-  if(!drag){
-    return;
-  }
-  event.PreventDefault();
-  drag=false;
-}
-
-let mouse_out = function(event){
-  event.PreventDefault();
-  if(!drag){
-    return;
-  }
-  event.PreventDefault();
-  drag=false;
-}
-
-let mouse_move = function(event){
-  event.PreventDefault();
-  if(!drag){
-    return;
-  }else{
-    event.PreventDefault();
-    let mouseX = parseInt(event.clientX);
-    let mouseY = parseInt(event.clientY);
-
-    let dx = mouseX - startX;
-    let dy = mouseX - startY;
-
-    console.log(dx,dy);
-    vierkant.x += dx;
-    vierkant.y += dy;
-
-    //clear it and redraw
-    ctx.clearRect(0,0,650,500);
-    vierkant= new component(30, 30, "orange", 10, 120);
-    
-    startX = mouseX;
-    startY = mouseY;
-
-  }
-}
-
-//eventlisteners
-canvas.onmousedown = mouse_down;
-canvas.onmouseup = mouse_up;
-canvas.onmouseout = mouse_out;
-canvas.onmousemove = mouse_move;
+*/
